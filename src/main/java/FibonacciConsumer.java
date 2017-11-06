fa36647595ac7c60d53e6c2a583f1e4d165a44c9
Markiian Yuskevych <markiyan.yuskevych@gmail.com>
2017-11-06 15:41:54 +0200

    added  output of sum of fibonacci numbers and original row depending on m-th input

--- src/main/java/FibonacciConsumer.java
+++ src/main/java/FibonacciConsumer.java
@@ -1,8 +1,5 @@
 import jnr.ffi.annotations.In;
-import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
@@ -10,14 +7,15 @@ import java.util.Collections;
 import java.util.Properties;
 
 public class FibonacciConsumer {
-    private final static String TOPIC ="test";
+    private final static String TOPIC = "test";
     private final static String BOOTSTRAP_SERVERS =
             "localhost:9092,localhost:9093,localhost:9094";
 
 
     public static void main(String[] args) throws InterruptedException {
-            runConsumer();
+        runConsumer(args[0]);
     }
+
     private static Consumer<Long, String> createConsumer() {
         final Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
@@ -39,35 +37,34 @@ public class FibonacciConsumer {
     }
 
 
-
-
-
-    static void runConsumer() throws InterruptedException {
+    static void runConsumer(String param) throws InterruptedException {
         final Consumer<Long, String> consumer = createConsumer();
-
-        final int giveUp = 100;   int noRecordsCount = 0;
-
+        int count = 1;
+        final int giveUp = 100;
+        int noRecordsCount = 0;
+        int m = Integer.parseInt(param);
         while (true) {
             final ConsumerRecords<Long, String> consumerRecords =
                     consumer.poll(1000);
 
-            if (consumerRecords.count()==0) {
+            if (consumerRecords.count() == 0) {
                 noRecordsCount++;
                 if (noRecordsCount > giveUp) break;
                 else continue;
             }
+            for (ConsumerRecord cr : consumerRecords) {
+                if (count % m == 0) {
+                    System.out.println(cr.value());
+                } else
+                    System.out.println(parseLine(cr.value().toString()));
 
-            consumerRecords.forEach(record -> {
-                System.out.printf("Consumer Record:(%d, %s, %d, %d)\n",
-                        record.key(), parseLine(record.value()),
-                        record.partition(), record.offset());
-            });
-
+                count++;
+            }
             consumer.commitAsync();
         }
     }
 
-    public static Integer parseLine(String line){
+    public static Integer parseLine(String line) {
         String[] s = line.split(" ");
         Integer sum = 0;
         for (int i = 0; i < s.length; i++) {
